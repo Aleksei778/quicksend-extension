6807934cbb2ccb53b7a03a8526951dcfe42a9f41
AlekseyPr <test@MacBook-Air-test.local>
2025-11-04 17:56:51 +0300

    init

---
+++ components/QuickSendButton.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+interface QuickSendButtonProps {
+    onClick: () => Promise<void>;
+}
+
+export function QuickSendButton({ onClick }: QuickSendButtonProps) {
+    const [disabled, setDisabled] = useState<boolean>(false);
+
+    const handleClick = async () => {
+        setDisabled(true);
+        await onClick();
+        setTimeout(() => setDisabled(false), 100);
+    }
+
+    return (
+        <button
+          className="ml-2 bg-blue-600 text-whire px-4 py-2 rounded disabled:bg-grey-400 relative"
+          onClick={handleClick}
+          disabled={disabled}
+          title={"Click to this Quicksend button instead of Send to send mass emails."}
+        >
+          Quicksend
+        </button>
+    )
+}
